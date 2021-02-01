aff3585843defff5949e589be52dcb7abafa2736
Henri Rofiq <henri.rofiq@mail.mcgill.ca>
2021-02-01 15:37:46 -0500

    Tutorial 2 practice

---
+++ EventRegistration-Backend/src/test/java/ca/mcgill/ecse1/eventregistration/EventRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse1.eventregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
